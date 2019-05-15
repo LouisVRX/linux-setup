075ea2a192e89a6a30343e0289f018c038d08a8d
LouisVRX <LouisVRX@users.noreply.github.com>
2019-05-15 12:26:27 -0400

    first version

---
+++ setup_linux.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+#
+sudo apt-get install -y git vim apt-transport-https ca-certificates curl software-properties-common htop
+
+#fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+#ssh key
+ssh-keygen -t rsa -C "louis@realisations.net"
+
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install docker-ce docker-compose -y
+sudo usermod -aG docker $USER
+
+# Python
+sudo apt-get install -y python-pip python3 python3-pip bpython3 python3-numpy python3-opencv
+python3 -m pip install --user vex
+python3 -m pip install tornado setuptools pandas matplotlib seaborn jedi cython
+export PYTHONSTARTUP="$(python3 -m jedi repl)"
